1a38467b5f43ff0611f76b4d9fd57bbf6ecb2aeb
Niranjan <kadu.s.niranjan@gmail.com>
2023-03-18 02:01:21 +0530

    code organized

--- calculator.js
+++ calculator.js
@@ -1,36 +1,44 @@
 let numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
 window.addEventListener("load", () => {
   const container = document.getElementById("numbers");
-  displayNumbers();
-  function displayNumbers() {
-    let count = 0;
-    for (let nos of numbers) {
-      count++;
-      const button = document.createElement("button");
-      button.value = nos;
-      button.classList.add("cal-button");
-      const node = document.createTextNode(nos);
-      button.appendChild(node);
-      const element = document.getElementById("numbers");
-      element.appendChild(button);
-      if (count % 3 == 0) {
-        element.appendChild(document.createElement("br"));
-      }
+  displayNumbers(container);
+  mouseClickOnDigits(container);
+  addClearButton(container);
+});
+
+function displayNumbers() {
+  let count = 0;
+  for (let nos of numbers) {
+    count++;
+    const button = document.createElement("button");
+    button.value = nos;
+    button.classList.add("cal-button");
+    const node = document.createTextNode(nos);
+    button.appendChild(node);
+    const element = document.getElementById("numbers");
+    element.appendChild(button);
+    if (count % 3 == 0) {
+      element.appendChild(document.createElement("br"));
     }
   }
-  container.addEventListener("click", function (e) {
-    if (e.target.classList.contains("cal-button")) {
-      document.getElementById("enter").value += e.target.value;
-    }
-  });
+}
+
+function addClearButton(container) {
   const clear = document.createElement("button");
   clear.appendChild(document.createTextNode("clear"));
   clear.classList.add("clr-button");
   clear.id = "clear-input";
   container.appendChild(clear);
-
   const clearBtn = document.getElementById("clear-input");
   clearBtn.addEventListener("click", function (e) {
     document.getElementById("enter").value = "";
   });
-});
+}
+
+function mouseClickOnDigits(container) {
+  container.addEventListener("click", function (e) {
+    if (e.target.classList.contains("cal-button")) {
+      document.getElementById("enter").value += e.target.value;
+    }
+  });
+}
